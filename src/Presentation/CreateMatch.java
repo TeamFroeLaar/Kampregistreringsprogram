bdeea58aa8dd8827c5da5782694bd42c4cb6b973
Dennis <Dennis@pcname.ceuherning.int>
2017-01-09 12:19:10 +0100

    I did some shit

--- src/Presentation/CreateMatch.java
+++ src/Presentation/CreateMatch.java
@@ -1,12 +1,19 @@
 package Presentation;
 
+
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
 public class CreateMatch {
@@ -27,15 +34,50 @@ public class CreateMatch {
 		grid.setVgap(10);
 		grid.setPadding(new Insets(25, 25, 25, 25));
 		
+		//Labels n shit
+		Label hjemmebane = new Label("Hjemme Bane");
+		grid.add(hjemmebane, 0, 1);
+		Label udebane = new Label("Ude Bane");
+		grid.add(udebane, 1, 1);
+		
+		//DropDown (hjemmebane)
+		ObservableList<String> hjemmeoptions = 
+				FXCollections.observableArrayList("hold");
+		
+			final ComboBox holdoptions = new ComboBox(hjemmeoptions);
+			HBox holdfelt = new HBox();
+			
+			holdfelt.getChildren().add(holdoptions);
+			grid.add(holdfelt, 0, 3);
+		
+		//DropDown (udebane)
+		ObservableList<String> udeoptions = 
+				FXCollections.observableArrayList("hold");
+			
+			final ComboBox uholdoptions = new ComboBox(udeoptions);
+			HBox uholdfelt = new HBox();
+				
+			uholdfelt.getChildren().add(uholdoptions);
+			grid.add(uholdfelt, 1, 3);
+		
 		//Buttons
 		Button tilbage = new Button("return");
-		grid.add(tilbage, 0, 0);
+		grid.add(tilbage, 0, 5);
 		tilbage.setOnAction(new EventHandler<ActionEvent>() {	
 			@Override
 			public void handle(ActionEvent event) {
 				Main view = new Main();
 				view.start(stage);
 			}
+		});
+		
+		Button opret = new Button("opret");
+		grid.add(opret, 1, 5);
+		opret.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event) {
+				
+		}
 		});
 		
 		Scene creatematch = new Scene(grid, 400, 375);
