c85595ee6e197221ed11700a6cdd856087413a0b
Morten <Morten@Mortens-Pc.ceuherning.int>
2017-01-09 09:31:09 +0100

    main fix

--- src/Presentation/Main.java
+++ src/Presentation/Main.java
@@ -3,14 +3,10 @@ package Presentation;
 import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
-import javafx.stage.Stage;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
-import javafx.scene.text.Font;
-import javafx.scene.text.FontWeight;
-import javafx.scene.text.Text;
+import javafx.stage.Stage;
 
 
 public class Main extends Application {
