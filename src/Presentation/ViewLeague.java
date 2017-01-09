56ed4abcdc9c2915bb808207bf59e07540da2a5b
Dennis <Dennis@pcname.ceuherning.int>
2017-01-09 12:54:58 +0100

    a

--- src/Presentation/ViewLeague.java
+++ src/Presentation/ViewLeague.java
@@ -3,7 +3,9 @@ package Presentation;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
@@ -29,6 +31,22 @@ public class ViewLeague {
 		TableView<Hold> table = new TableView<>();
 		table.setEditable(true);
 		table.setItems(data);
+		
+		//TableView Rækker
+		TableColumn<Hold, String> stillingCol = new TableColumn<Hold, String>("Stilling");
+		stillingCol.setCellValueFactory(new PropertyValueFactory<Hold, String>("stilling");
+		
+		TableColumn<Hold, String> holdnavnCol = new TableColumn<Hold, String>("Hold navn");
+		holdnavnCol.setCellValueFactory(new PropertyValueFactory<Hold, String>("holdnavn");
+		
+		TableColumn<Hold, String> maalCol = new TableColumn<Hold, String>("Mål");
+		maalCol.setCellValueFactory(new PropertyValueFactory<Hold, String>("mål");
+		
+		TableColumn<Hold, String> udvisningsCol = new TableColumn<Hold, String>("Udvisninger");
+		udvisningsCol.setCellValueFactory(new PropertyValueFactory<Hold, String>("udvisninger");
+		
+		TableColumn<Hold, String> pointCol = new TableColumn<Hold, String>("Points");
+		pointCol.setCellValueFactory(new PropertyValueFactory<Hold, String>("points");
 		
 		Scene viewleague = new Scene(grid, 400, 375);
 		stage.setScene(viewleague);
