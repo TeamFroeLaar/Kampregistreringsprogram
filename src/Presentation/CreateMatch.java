f28236139a44b4a3c504419bf49e130f65be10c4
Morten <Morten@Mortens-Pc.ceuherning.int>
2017-01-10 09:01:49 +0100

    acs

--- src/Presentation/CreateMatch.java
+++ src/Presentation/CreateMatch.java
@@ -17,67 +17,64 @@ import javafx.stage.Stage;
 public class CreateMatch {
 	private Stage stage;
 	private GridPane grid;
-	
+
 	public CreateMatch(Stage stage) {
 		this.stage = stage;
-	
+
 	}
 
-	public void init() 
-	{
+	public void init() {
 		stage.setTitle("Create a match");
 		grid = new GridPane();
 		grid.setAlignment(Pos.TOP_CENTER);
 		grid.setHgap(10);
 		grid.setVgap(10);
 		grid.setPadding(new Insets(25, 25, 25, 25));
-		
-		//Labels n shit
+
+		// Labels n shit
 		Label hjemmebane = new Label("Hjemme Bane");
 		grid.add(hjemmebane, 0, 1);
 		Label udebane = new Label("Ude Bane");
 		grid.add(udebane, 1, 1);
-		
-		//DropDown (hjemmebane)
-		ObservableList<String> hjemmeoptions = 
-				FXCollections.observableArrayList("hold");
-		
-			final ComboBox holdoptions = new ComboBox(hjemmeoptions);
-			HBox holdfelt = new HBox();
-			
-			holdfelt.getChildren().add(holdoptions);
-			grid.add(holdfelt, 0, 3);
-		
-		//DropDown (udebane)
-		ObservableList<String> udeoptions = 
-				FXCollections.observableArrayList("hold");
-			
-			final ComboBox uholdoptions = new ComboBox(udeoptions);
-			HBox uholdfelt = new HBox();
-				
-			uholdfelt.getChildren().add(uholdoptions);
-			grid.add(uholdfelt, 1, 3);
-		
-		//Buttons
+
+		// DropDown (hjemmebane)
+		ObservableList<String> hjemmeoptions = FXCollections.observableArrayList("hold");
+
+		final ComboBox holdoptions = new ComboBox(hjemmeoptions);
+		HBox holdfelt = new HBox();
+
+		holdfelt.getChildren().add(holdoptions);
+		grid.add(holdfelt, 0, 3);
+
+		// DropDown (udebane)
+		ObservableList<String> udeoptions = FXCollections.observableArrayList("hold");
+
+		final ComboBox uholdoptions = new ComboBox(udeoptions);
+		HBox uholdfelt = new HBox();
+
+		uholdfelt.getChildren().add(uholdoptions);
+		grid.add(uholdfelt, 1, 3);
+
+		// Buttons
 		Button tilbage = new Button("return");
 		grid.add(tilbage, 0, 5);
-		tilbage.setOnAction(new EventHandler<ActionEvent>() {	
+		tilbage.setOnAction(new EventHandler<ActionEvent>() {
 			@Override
 			public void handle(ActionEvent event) {
 				Main view = new Main();
 				view.start(stage);
 			}
 		});
-		
+
 		Button opret = new Button("opret");
 		grid.add(opret, 1, 5);
 		opret.setOnAction(new EventHandler<ActionEvent>() {
 			@Override
 			public void handle(ActionEvent event) {
-				
-		}
+
+			}
 		});
-		
+
 		Scene creatematch = new Scene(grid, 400, 375);
 		stage.setScene(creatematch);
 		stage.show();
