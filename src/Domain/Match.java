848d9f92f8d7d40c13a3db695c8f046a0e81ba65
Dennis <Dennis@pcname.ceuherning.int>
2017-01-10 08:47:45 +0100

    s

---
+++ src/Domain/Match.java
@@ -0,0 +1,35 @@
+package Domain;
+
+public class Match 
+{
+	private String id;
+	private String hjemmeholdId;
+	private String udeholdId;
+	private String datoTid;
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getHjemmeholdId() {
+		return hjemmeholdId;
+	}
+	public void setHjemmeholdId(String hjemmeholdId) {
+		this.hjemmeholdId = hjemmeholdId;
+	}
+	public String getUdeholdId() {
+		return udeholdId;
+	}
+	public void setUdeholdId(String udeholdId) {
+		this.udeholdId = udeholdId;
+	}
+	public String getDatoTid() {
+		return datoTid;
+	}
+	public void setDatoTid(String datoTid) {
+		this.datoTid = datoTid;
+	}
+
+
+}
---
+++ src/Domain/Team.java
@@ -0,0 +1,21 @@
+package Domain;
+
+public class Team 
+{
+	private String id;
+	private String holdnavn;
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getHoldnavn() {
+		return holdnavn;
+	}
+	public void setHoldnavn(String holdnavn) {
+		this.holdnavn = holdnavn;
+	}
+	
+}
