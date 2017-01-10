495749ccb976d1da70a31952ef927a53625b1326
Morten <Morten@Mortens-Pc.ceuherning.int>
2017-01-10 08:28:25 +0100

    kj

--- src/Presentation/ViewLeague.java
+++ src/Presentation/ViewLeague.java
@@ -6,9 +6,6 @@ import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
