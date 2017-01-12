4ebc2bbc2da66033b182b8179a244298624175d4
Morten <Morten@10.153.244.42>
2017-01-12 09:12:28 +0100

    selectTeam

---
+++ src/Data/SelectTeamDB.java
@@ -0,0 +1,41 @@
+package Data;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import Domain.Team;
+
+public class SelectTeamDB {
+	public List<Team> SelectTeam(Team team) {
+		List<Team> list = new ArrayList<>();
+		try (Connection connection = DriverManager.getConnection("jdbc:hsqldb:hsql://localhost/kampReg", "SA", "");
+				PreparedStatement statement = connection.prepareStatement("SELECT holdnavn,id from hold");) {
+
+			ResultSet rs = statement.executeQuery();
+			try {
+				connection.setAutoCommit(false);
+
+				//while løkke
+				Team t = new Team();
+				t.setHoldnavn(rs.getString("HOLDNAVN"));
+				t.setId(rs.getString("ID"));
+				list.add(t);
+
+				int antal = statement.executeUpdate();
+				System.out.println("Antal rækker berørt : " + antal);
+				connection.commit();
+			} catch (SQLException e) {
+				System.out.println("Fejl ved søgning");
+				connection.rollback();
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return list;
+	}
+}
