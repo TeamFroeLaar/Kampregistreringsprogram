29b57ffe2dc190d474c90a0e9fc3511b58860529
Morten <Morten@10.153.244.42>
2017-01-12 12:23:47 +0100

    FUCK THIS SHIT AGAIN

--- src/Domain/Team.java
+++ src/Domain/Team.java
@@ -1,21 +1,27 @@
 package Domain;
 
-public class Team 
-{
+public class Team {
 	private String id;
 	private String holdnavn;
-	
+
 	public String getId() {
 		return id;
 	}
+
 	public void setId(String id) {
 		this.id = id;
 	}
+
 	public String getHoldnavn() {
 		return holdnavn;
 	}
+
 	public void setHoldnavn(String holdnavn) {
 		this.holdnavn = holdnavn;
 	}
-	
+
+	@Override
+	public String toString() {
+		return holdnavn;
+	}
 }
