e22c5a1b53891ad61426975069fbbcf121d73500
Allan Strange <alnstrange@gmail.com>
2017-01-12 11:07:36 +0100

    hmm

--- src/Presentation/ViewMatch.java
+++ src/Presentation/ViewMatch.java
@@ -7,6 +7,10 @@ import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class ViewMatch {
@@ -41,8 +45,43 @@ public class ViewMatch {
 				awayTeamGrid.setHgap(10);
 				awayTeamGrid.setVgap(10);
 				awayTeamGrid.setPadding(new Insets(25, 25, 25, 25));
-				grid.add(awayTeamGrid, 0, 2);
+				grid.add(awayTeamGrid, 2, 0);
+				
+				// gridlines
+				grid.setGridLinesVisible(true);
+				homeTeamGrid.setGridLinesVisible(true);
+				awayTeamGrid.setGridLinesVisible(true);
+				
+				// boxes with scores
+				Rectangle rectangleLeft = new Rectangle(75,75,75,75);
+				rectangleLeft.setStroke(Color.BLACK);
+				rectangleLeft.setFill(null);
+				rectangleLeft.setStrokeWidth(3);
+				
+				Rectangle rectangleRight = new Rectangle(75, 75, 75, 75);
+				rectangleRight.setStroke(Color.BLACK);
+				rectangleRight.setFill(null);
+				rectangleRight.setStrokeWidth(3);
 		
+// scores --------------------------------------------------- TO BE CHANGED				
+				Text scoreRight = new Text("0");
+				Text scoreLeft = new Text("0");
+				
+				// adds rectangles and scores to stackpane
+				StackPane stackHomeTeam = new StackPane();
+				stackHomeTeam.getChildren().addAll(rectangleLeft, scoreLeft);
+				
+				StackPane stackAwayTeam = new StackPane();
+				stackAwayTeam.getChildren().addAll(rectangleRight, scoreRight);
+				
+				// home team match info
+				Text homeTeam = new Text("Home");
+				homeTeamGrid.add(homeTeam, 0, 0);
+				Text twoMinutePenalty = new Text("2-min penalty");
+				homeTeamGrid.add(twoMinutePenalty, 0, 1);
+				
+				
+				
 		Button tilbage = new Button("Return");
 		grid.add(tilbage, 0, 1);
 		tilbage.setOnAction(new EventHandler<ActionEvent>() {	
