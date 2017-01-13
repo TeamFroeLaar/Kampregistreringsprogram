358cd0e467ac7fb46f6bb1752bae41bd7cff312e
Dennis <Dennis@pcname.ceuherning.int>
2017-01-13 11:52:01 +0100

    DataAccess Klasse

---
+++ src/Data/DataAccess.java
@@ -0,0 +1,48 @@
+package Data;
+
+import java.io.Closeable;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DataAccess implements Closeable {
+	
+	private Connection conn;
+	
+	public DataAccess() {
+		try {
+			this.conn =	DriverManager.getConnection("jdbc:hsqldb:hsql://localhost/kampreg", "SA", "");
+			this.conn.setAutoCommit(false);
+		} catch (SQLException e) {
+			throw new RuntimeException("Connection could not be established", e);
+		}
+	}
+	
+	public void commit() {
+		try {
+			this.conn.commit();
+		} catch (SQLException e) {
+			throw new RuntimeException("Exception caught", e);
+		}
+	}
+
+	public void rollback() {
+		try {
+			this.conn.rollback();
+		} catch (SQLException e) {
+			throw new RuntimeException("Exception caught", e);
+		}
+	}
+	
+	public void close() {
+		try {
+			this.conn.close();
+		} catch (SQLException e) {
+			throw new RuntimeException("Exception caught", e);
+		}
+	}
+	
+	public Connection getConnection() {
+		return this.conn;
+	}
+}
