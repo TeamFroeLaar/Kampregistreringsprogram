893a106c1df882ff3cef375c543a1fdc2ce4e422
Morten <Morten@Mortens-Pc.ceuherning.int>
2017-01-13 09:38:36 +0100

    teat

--- src/Data/DBfacade.java
+++ src/Data/DBfacade.java
@@ -1,29 +1,37 @@
 package Data;
 
+import java.util.List;
+
 import Domain.Match;
 import Domain.Team;
 
-public class DBfacade 
-{
+public class DBfacade {
+	// Create Match
 	private CreateMatchDB createMatchInfo = new CreateMatchDB();
 
-	public void createMatchInfo(Match match) 
-	{
+	public void createMatchInfo(Match match) {
 		createMatchInfo.createMatch(match);
 	}
+
+	// Select Macth
 	private SelectMatchDB selectMatchInfo = new SelectMatchDB();
-	
-	public void selectMatchInfo() 
-	{
+
+	public void selectMatchInfo() {
 		selectMatchInfo.selectMatch();
 	}
 
+	// Create Team
 	private CreateTeamDB createTeamInfo = new CreateTeamDB();
 
-	public void createTeamInfo(Team team) 
-	{
+	public void createTeamInfo(Team team) {
 		createTeamInfo.createTeam(team);
 	}
 
+	// Select Team
+	private SelectTeamDB selectTeamInfo = new SelectTeamDB();
+
+	public List<Team> selectTeamInfo() {
+		return selectTeamInfo.selectTeam();
+	}
+
 }
- 
