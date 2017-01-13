177564cf86bb230cddbf4a21c11db82110f14a2f
Dennis <Dennis@pcname.ceuherning.int>
2017-01-13 09:34:34 +0100

    yes

--- src/Data/DBfacade.java
+++ src/Data/DBfacade.java
@@ -3,16 +3,25 @@ package Data;
 import Domain.Match;
 import Domain.Team;
 
-public class DBfacade {
+public class DBfacade 
+{
 	private CreateMatchDB createMatchInfo = new CreateMatchDB();
 
-	public void createMatchInfo(Match match) {
+	public void createMatchInfo(Match match) 
+	{
 		createMatchInfo.createMatch(match);
+	}
+	private SelectMatchDB selectMatchInfo = new SelectMatchDB();
+	
+	public void selectMatchInfo() 
+	{
+		selectMatchInfo.selectMatch();
 	}
 
 	private CreateTeamDB createTeamInfo = new CreateTeamDB();
 
-	public void createTeamInfo(Team team) {
+	public void createTeamInfo(Team team) 
+	{
 		createTeamInfo.createTeam(team);
 	}
 
