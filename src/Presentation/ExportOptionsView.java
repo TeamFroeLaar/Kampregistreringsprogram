a17b7ca95e1fc723414e1084537045470df37091
Allan Strange <alnstrange@gmail.com>
2017-01-19 19:46:20 +0100

    Added Mikkel Hansen to Export

--- src/Presentation/ExportOptionsView.java
+++ src/Presentation/ExportOptionsView.java
@@ -9,6 +9,8 @@ import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
@@ -29,9 +31,21 @@ public class ExportOptionsView {
 		grid.setHgap(25);
 		grid.setVgap(10);
 		grid.setPadding(new Insets(25, 25, 25, 25));
+		
+		
+		// Mikkel Hansen
+		Image mikkelHansen = new Image("Presentation/mikkel.png");
+		ImageView imageView = new ImageView();
+		imageView.setImage(mikkelHansen);
+		imageView.setFitWidth(200);
+		imageView.setFitHeight(200);
+		imageView.setPreserveRatio(true);
+        imageView.setSmooth(true);
+        imageView.setCache(true);
+        grid.add(imageView, 1, 5);
 
 		// GridLine
-		grid.setGridLinesVisible(true);
+		grid.setGridLinesVisible(false);
 
 		// DropDown export
 		ObservableList<String> exportOptions = FXCollections.observableArrayList("TXT", "CSV", "PDF");
@@ -41,7 +55,8 @@ public class ExportOptionsView {
 		exportOptionsField.getChildren().add(cmbExportStrings);
 		cmbExportStrings.setValue("TXT");
 		grid.add(exportOptionsField, 1, 0);
-
+		exportOptionsField.setAlignment(Pos.CENTER);
+		
 		// Export
 		Button export = new Button("Export");
 		grid.add(export, 2, 0);
@@ -64,7 +79,7 @@ public class ExportOptionsView {
 			}
 		});
 
-		Scene viewExportOptions = new Scene(grid, 300, 325);
+		Scene viewExportOptions = new Scene(grid, 400, 375);
 		stage.setScene(viewExportOptions);
 		stage.show();
 
