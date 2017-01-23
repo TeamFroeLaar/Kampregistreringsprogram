138e0c1a6dc465362464fc237992011d5e565e84
Morten <Morten@Mortens-Pc.ceuherning.int>
2017-01-23 10:17:56 +0100

    select league

--- src/Data/SelectLeagueDB.java
+++ src/Data/SelectLeagueDB.java
@@ -3,13 +3,16 @@ package Data;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import Domain.League;
+import Domain.Match;
 
 public class SelectLeagueDB {
 	
-	public League selectLeague() {
-		League league = new League();
+	public List<League> selectLeague() {
+		List<League> leagueList = new ArrayList<>();
 		try (DataAccess access = new DataAccess()) {
 			try {
 				selectLeague();
@@ -19,21 +22,23 @@ public class SelectLeagueDB {
 				throw e;
 			}
 		}
-		return league;
+		return leagueList;
 	}
 
-	private void selectMatch(DataAccess access, League league, String id) {
+	private void selectMatch(DataAccess access, List<League> leagueList) {
 		try (PreparedStatement statement = access.getConnection()
 				.prepareStatement(/*insert SQL*/);) {
-			statement.setString(1, id);
+			statement.setString(1);
 			ResultSet rs = statement.executeQuery();
 			while (rs.next()) {
 //				league.setStilling(rs.getString(columnIndex));
-				league.setHoldnavn(rs.getString());
-				league.setEvent(rs.getString(columnIndex));
-				league.setWin(rs.getString(columnIndex));
-				league.setLoss(rs.getString(columnIndex));
-				league.setPoint(rs.getString(columnIndex));
+				League l = new League();
+				l.setHoldnavn(rs.getString());
+				l.setEvent(rs.getString(columnIndex));
+				l.setWin(rs.getString(columnIndex));
+				l.setLoss(rs.getString(columnIndex));
+				l.setPoint(rs.getString(columnIndex));
+				leagueList.add(l);
 			}
 		} catch (SQLException e) {
 			throw new RuntimeException("fejl ved søgning", e);
